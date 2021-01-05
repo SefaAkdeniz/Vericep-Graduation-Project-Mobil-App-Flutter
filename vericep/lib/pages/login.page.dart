444d5387e5854e8172ebb02c76ab688ec6efa0c4
İsmail Sefa <ismailsefa.akdnz@gmail.com>
2021-01-06 00:15:38 +0300

    create login page and widgets

---
+++ vericep/lib/pages/login.page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:vericep/widget/buttonLogin.dart';
+import 'package:vericep/widget/firstTime.dart';
+import 'package:vericep/widget/inputUsername.dart';
+import 'package:vericep/widget/inputPassword.dart';
+import 'package:vericep/widget/textLogin.dart';
+import 'package:vericep/widget/verticalText.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.blueGrey, Colors.lightGreenAccent]),
+        ),
+        child: ListView(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Row(children: <Widget>[
+                  VerticalText(),
+                  TextLogin(),
+                ]),
+                InputUsername(),
+                PasswordInput(),
+                ButtonLogin(),
+                FirstTime(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
