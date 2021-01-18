c21854eca6c91f6a481065008494836352674c63
İsmail Sefa <ismailsefa.akdnz@gmail.com>
2021-01-18 04:22:14 +0300

    add setBalance service method

--- vericep/lib/api/balanceServices.dart
+++ vericep/lib/api/balanceServices.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:vericep/models/response.dart';
 
 class BalanceServices {
   Future getBalance(String id) async {
@@ -20,6 +21,22 @@ class BalanceServices {
       } else {
         return "0.00";
       }
+    } else {
+      throw Exception('Failed request.');
+    }
+  }
+
+  static Future<Response> setBalance(user_id, process_price) async {
+    final http.Response response = await http.post(
+      'http://10.0.2.2:8000/payment/setBalance/',
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(
+          <String, String>{'user_id': user_id, 'process_price': process_price}),
+    );
+    if (response.statusCode == 200) {
+      return Response.fromJson(jsonDecode(response.body));
     } else {
       throw Exception('Failed request.');
     }
